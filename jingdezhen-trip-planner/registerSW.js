83bdc1200b833318467081b9307ce3cfe2b70e7f
willw <willw@users.noreply.github.com>
2026-06-12 23:20:19 +0800

    Deploy Jingdezhen trip planner

---
+++ jingdezhen-trip-planner/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/jingdezhen-trip-planner/sw.js', { scope: '/jingdezhen-trip-planner/' })})}
